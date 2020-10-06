21ce7bdb313f0ebc81e0469b32d4d174086ac0c9
Dhruva Shaw <dhruvashaw@gmail.com>
2020-10-06 20:31:31 +0530

    Added file upload

--- qrgen/main.py
+++ qrgen/main.py
@@ -1,11 +1,14 @@
 import os
 import tkinter as tk
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+from tkinter.ttk import Progressbar
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 #The main root class
 class Root(tk.Tk):
-    global BASE_DIR
+    global THIS_FOLDER
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.title("QR - Code Generator")
@@ -13,7 +16,7 @@ class Root(tk.Tk):
         self.minsize(644,644)
         self.maxsize(644,1200)
         
-        self.wm_iconbitmap(os.path.join(BASE_DIR, 'qrgen','static','images','logo.ico'))
+        self.wm_iconbitmap(os.path.join(THIS_FOLDER,'static','images','logo.ico'))
 
 
 #The Title frame
@@ -51,50 +54,90 @@ class EntryInput(tk.Frame):
         self.master = master
         self.pack()
         self.create_widgets()
+        self.logofilepath = ""
+    
+    #Choosing the image
+    def logoupload(self):
+        global THIS_FOLDER
+        self.logofilepath = askopenfilename(
+            initialdir = os.path.join(os.path.dirname(THIS_FOLDER),'logo'),
+            title = "choose your image file",
+            filetypes = (("jpeg files","*.jpg"),("all files","*.*"))
+        )
 
     def create_widgets(self):
-        from tkinter.ttk import Progressbar
+        #Label the dataentry
         self.data = tk.Label(
             self, 
             text="Data to be Encoded :",
             justify="left",
             font = ("Arial",18),
         )
+
+        #Progressbar to track the progress
         self.progress = Progressbar(
             self, 
             orient = 'horizontal', 
             length = 500, 
             mode = 'determinate'
         )
+
+        #Submit button
         self.submit = tk.Button(
             self,
             text="Generate QR-Code",
             font=("Arial",15,"bold"),
-            bg='white',
-            command=self.say_hi,
-            width=40
+            bg='#ebab34',
+            command=self.gen_qr_code,
+            width=40,
         )
 
+        #Initialisation of Tkinter Variables
         self.datavalue = tk.StringVar()
+        self.logo = tk.IntVar()
+        
+        #Entry widget of the tkinter
         self.datarentry = tk.Entry(
             self, 
             textvariable = self.datavalue,
             font = ("Times New Roman",18),
         )
-        self.data['pady'] = 34
+        self.data['height'] = 4
 
-        self.data.grid(row=0,column = 1, pady = 2, padx=2)
-        self.datarentry.grid(row=0,column = 2, pady = 2,padx=2)
-        self.progress.grid(row=1,column=1,columnspan=2)
-        self.submit.grid(row=2,column=1,columnspan=2,pady=25,padx=2)
-    
-    def say_hi(self):
+        #Checkbox for the center logo
+        self.logocheckbox = tk.Checkbutton(
+            self,
+            text="Want to put the logo at center?", 
+            variable = self.logo,
+            highlightcolor='#ebab34',
+            font=("Arial",15,"bold"),
+            command=self.logoupload
+        )
+
+        #Packing the elements in a grid
+        self.data.grid(row=0,column = 1, padx=2)
+        self.datarentry.grid(row=0,column = 2,padx=2)
+        self.logocheckbox.grid(row=1,column = 1,pady = 2,columnspan=2)        
+        self.progress.grid(row=2,column=1,columnspan=2)
+        self.submit.grid(row=3,column=1,columnspan=2,pady=25,padx=2)
+
+    #Qrcode generation function
+    def gen_qr_code(self):
+        import pyqrcode
+        print(self.logofilepath)
+
         #Getting the data
         title = self.datavalue.get()
-        print(title)
+        files_logo = os.listdir(os.path.join(os.path.dirname(THIS_FOLDER), 'logo'))
+        
+        #Error generation
+        if title in ('',None): 
+            messagebox.showerror('No Data Found!!!',"No data was given in order to get encoded, so please try again!!!")
+            return False
+        
         self.progress['value'] = 20
         self.update_idletasks()
-    
+
 
 #the Image display frame
 class QRCodeImageShow(tk.Frame):
@@ -108,7 +151,7 @@ class QRCodeImageShow(tk.Frame):
 #__main__
 if __name__ == '__main__':
     root = Root()
-    if not os.path.isdir(os.path.join('qrcode_images')): os.mkdir(os.path.join(BASE_DIR,'qrcode_images'))
+    if not os.path.isdir(os.path.join('qrcode_images')): os.mkdir(os.path.join(THIS_FOLDER,'qrcode_images'))
 
     title = Title(master=root)
     app = EntryInput(master=root)
